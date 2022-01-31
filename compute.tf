3c154c683ac9a86e9e1ca8d1804e38334984d943
janaom <83917694+janaom@users.noreply.github.com>
2022-01-31 22:27:02 +0200

    Create compute.tf

---
+++ compute.tf
@@ -0,0 +1,31 @@
+data "aws_ami" "server_ami" {
+    most_recent = true
+    owners = [""]
+    filter {
+         name = "name"
+         values = ["-*"]
+
+}
+}
+resource "random_id" "my_node_id" {
+    byte_length = 2
+    count = var.main_instance_count
+}
+
+resource "aws_instance" "my_main" {
+    count = var.main_instance_count
+    instance_type = var.main_instance_type
+    ami = data.aws_ami.server_ami.id
+    #key_name = ""
+    vpc_security_group_ids = [aws_security_group.my_sg.id]
+    subnet_id = aws_subnet.my_public_subnet[0].id
+    root_block_device {
+          volume_size = var.main_vol_size
+
+}
+
+    tags = {
+       Name = "my-main-${random_id.my_node_id[count.index].dec}"
+       Owner = "Jana"
+}
+}
