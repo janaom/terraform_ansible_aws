972c0b13cc23cc383b47e4a2fac08af6364c3817
janaom <83917694+janaom@users.noreply.github.com>
2022-02-06 19:52:08 +0200

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -1,9 +1,9 @@
 data "aws_ami" "server_ami" {
     most_recent = true
-    owners = ["099720109477"] #to know the Owner ID, run: sudo apt install awscli; then aws configure; then aws ec2 describe-images --image-ids ami-0d527b8c289b4af7f --region eu-central-1
+    owners = ["099720109477"]
     filter {
          name = "name"
-         values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-20211129-*"] #to find the name, check the comment on 'owners'
+         values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-20211129-*"]
 
 }
 }
@@ -12,13 +12,18 @@ resource "random_id" "my_node_id" {
     count = var.main_instance_count
 }
 
+resource "aws_key_pair" "my_auth" {
+    key_name = var.key_name
+    public_key = file(car.public_key_path)
+}
 resource "aws_instance" "my_main" {
     count = var.main_instance_count
     instance_type = var.main_instance_type
     ami = data.aws_ami.server_ami.id
-    #key_name = ""
+    key_name = aws_key_pair.my_auth.id
     vpc_security_group_ids = [aws_security_group.my_sg.id]
-    subnet_id = aws_subnet.my_public_subnet[0].id
+    subnet_id = aws_subnet.my_public_subnet[count.index].id
+    user_data = templatefile("./main-userdata.tpl", {new_hostname = "my-main-${random_id.my_node_id[count.index].dec}"})
     root_block_device {
           volume_size = var.main_vol_size
 
@@ -27,5 +32,27 @@ resource "aws_instance" "my_main" {
     tags = {
        Name = "my-main-${random_id.my_node_id[count.index].dec}"
        Owner = "Jana"
+}
+
+    provisioner "local-exec" {
+         command = "printf '\n${self.public_ip}' >> aws_hosts"
+}
+    provisioner "local-exec" {
+        when = destroy
+        command = "sed -i '/^[0-9]/d' aws_hosts"
+}
+}
+
+resource "null_resource" "grafana_update" {
+        count = var.main_instance_count
+        provisioner "remote-exec" {
+            inline = ["sudo apt upgrade -y grafana && touch upgrade.log && echo 'I updated Grafana' >> upgrade.log"]
+
+        connection {
+            type = "ssh"
+            user = "ubuntu"
+            private_key = file("./terraform.pem")
+            host = aws_instance.my_main[count.index].public_ip
+}
 }
 }
